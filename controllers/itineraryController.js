291c62b0ecfc7533fd6990abeafa90c081b84dde
ahammed-ihsan <ihsanahammed908@gmail.com>
2026-05-23 21:19:53 +0530

    added sharing functionalities

--- controllers/itineraryController.js
+++ controllers/itineraryController.js
@@ -1,4 +1,5 @@
 const itineraryModel = require("../models/itineraryModel");
+const { nanoid } = require("nanoid");
 
 const getItinerary = async (req, res) => {
     try {
@@ -22,4 +23,38 @@ const getAllItineraries = async (req, res) => {
     }
 };
 
-module.exports = { getItinerary, getAllItineraries };
+const createItineraryShareLink = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const itinerary = await itineraryModel.findOne({ _id: id, user: req.user.id });
+        if (!itinerary) {
+            return res.status(404).json({ message: "Itinerary not found" });
+        }
+
+        const shareId = nanoid(10);
+
+        itinerary.shareId = shareId;
+        itinerary.isPublic = true;
+
+        await itinerary.save();
+        
+        res.json({ shareLink: `https://ai-itinerary-generator-frontend-ihw.vercel.app/shared/${itinerary.shareId}` });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getItineraryByShareId = async (req, res) => {
+    try {
+        const shareId = req.params.shareId;
+        const itinerary = await itineraryModel.findOne({ shareId, isPublic: true });
+        if (!itinerary) {
+            return res.status(404).json({ message: "Itinerary not found" });
+        }
+        res.json(itinerary);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { getItinerary, getAllItineraries, createItineraryShareLink, getItineraryByShareId };
--- routes/itineraryRoute.js
+++ routes/itineraryRoute.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { getItinerary, getAllItineraries } = require("../controllers/itineraryController");
+const { getItinerary, getAllItineraries, createItineraryShareLink, getItineraryByShareId } = require("../controllers/itineraryController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get('/:id', authMiddleware, getItinerary);
 router.get('/', authMiddleware, getAllItineraries);
+router.post('/:id/share', authMiddleware, createItineraryShareLink);
+router.get('/share/:shareId', getItineraryByShareId);
 
 module.exports = router;
